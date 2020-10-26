57776642a91fabcd0472770fc54803f3b453e9a6
Parvathy <parvathys0311@gmail.com>
2020-10-25 20:58:37 -0600

    wsgi tweaks

--- testDeployProject/wsgi.py
+++ testDeployProject/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testDeployProject.settings')
 
